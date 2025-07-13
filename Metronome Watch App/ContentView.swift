caeb8fe8ad9f13e9f3a74d78acdd14d75b302cf6
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2025-07-13 14:21:01 -0400

    basic metronome, no accents, but it taps when the screen dims

--- Metronome Watch App/ContentView.swift
+++ Metronome Watch App/ContentView.swift
@@ -8,16 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var beatsPerMinute: Double = 60
+    @State private var isRunning: Bool = false
+    @State private var timer: Timer?
+    @State private var session: WKExtendedRuntimeSession?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("BPM: \(Int(beatsPerMinute))")
+            Button(action: {
+                enableMetronome()
+            }) {
+                Text(isRunning ? "Stop" : "Start").frame(width: 100)
+            }.buttonStyle(.borderedProminent)
+        }.focusable(true)
+            .digitalCrownRotation(
+                $beatsPerMinute,
+                from: 20,
+                through: 200,
+                by: 1,
+                sensitivity: .medium,
+                isHapticFeedbackEnabled: true
+            ).onChange(of: beatsPerMinute) {
+                if isRunning {
+                    restartTick()
+                }
+            }
         .padding()
     }
+    private func startTick() {
+        let interval = 60.0 / beatsPerMinute
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+            WKInterfaceDevice.current().play(.start)
+        }
+        
+    }
+    private func restartTick() {
+        timer?.invalidate()
+        startTick()
+    }
+    private func enableMetronome() {
+        isRunning.toggle()
+        if isRunning {
+            startTick()
+            session?.start()
+        } else {
+            timer?.invalidate()
+            timer = nil
+            session?.invalidate()
+        }
+    }
 }
+
+
 
 #Preview {
     ContentView()
